ad53e0e33d0cac2ca7d4ca6f09cd79384583348d
Pouya barari <b_pooya@yahoo.com>
2023-03-06 13:07:20 +0330

    redux dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,12 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./RootReducer";
-
+import { composeWithDevTools } from "redux-devtools-extension";
 const middleware = [thunk, logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
